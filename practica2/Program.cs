1750943bfa5cd6c8fb6f79af9db4a63b8836e4ad
leonardo4390 <tidus4390@gmail.com>
2025-07-21 10:37:56 -0300

    manejos de archivos

---
+++ practica2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+
+    //escibiendo y leyendo lineas de textos
+    public static void Main(String[] args)
+    {
+        string ruta = "datos.txt";//direccion del archivo
+        if (File.Exists("datos.txt"))//verificacion del la existencia del archivo
+        {
+            Console.WriteLine("\nExiste el archivo\n");//agregar linea
+            File.AppendAllText(ruta, "\nOtra linea");//agregar otra linea
+            string contenido = File.ReadAllText(ruta);//leer todo el archivo
+            Console.WriteLine(contenido);
+            string[] lineas = File.ReadAllLines(ruta);//leer linea por linea
+            foreach (string linea in lineas)
+            {
+                Console.WriteLine(linea);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("\nNo existe el archivo, creando archivo\n");
+
+            File.WriteAllText(ruta, "Primera linea");
+
+        }
+        
+
+    }
+}
