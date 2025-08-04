88c4f012efaaa1df2ecf937f273a14784abbe652
leonardo4390 <tidus4390@gmail.com>
2025-08-04 10:12:16 -0300

    abstraccion ejemplos

---
+++ practica5/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Abstraccion*/
+using System;
+using System.Runtime.InteropServices;
+// clases abstractas
+// abstract class Animal
+// {
+//     public abstract void HacerSonido();
+
+//     public void Dormir()
+//     {
+//         Console.WriteLine("Zzzz...");
+//     }
+
+// }
+
+// class Perro : Animal
+// {
+//     public override void HacerSonido()
+//     {
+//         Console.WriteLine("Guau guau");
+//     }
+// }
+
+/*interfaces*/
+class Program
+{
+
+    // public static void Main(String[] args)
+    // {
+    //     Animal animal = new Perro();
+    //     animal.HacerSonido();
+    //     animal.Dormir();
+    // }
+    interface Ivehiculo
+    {
+        void Conducir();
+        int velocidadMaxima{ get; }
+    }
+
+    class Auto : Ivehiculo
+    {
+        public int velocidadMaxima => 100;
+
+        public void Conducir()
+        {
+            Console.WriteLine("Conduciendo el auto...");
+        }
+    }
+    public static void Main(String[] args)
+    {
+        Ivehiculo vehiculo = new Auto();
+        vehiculo.Conducir();
+        Console.WriteLine(vehiculo.velocidadMaxima);
+    }
+}
